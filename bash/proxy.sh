1161b41f4f1da17193c9f2e7a7f4d301f4e5600a
objout <android-ios@qq.com>
2025-12-24 00:28:53 +0800

    Add bash/proxy-env.sh.

--- bash/proxy.sh
+++ bash/proxy.sh
@@ -1,8 +1,3 @@
-export HTTP_PROXY="socks5://127.0.0.1:7890"
-export HTTPS_PROXY="socks5://127.0.0.1:7890"
-export ALL_PROXY="socks5://127.0.0.1:7890"
-export NO_PROXY="localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
-
 set_proxy() {
   local proxy_addr=${1:-"socks5://127.0.0.1:7890"}
 
