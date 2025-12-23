2efa2a65c1d74a61bac49547a75496a57938a737
objout <android-ios@qq.com>
2025-12-24 00:24:04 +0800

    Update get.sh.

--- get.sh
+++ get.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-SITE="gitee.com"
-USER_NAME="objout"
-REPO_NAME="cs"
-BRANCH="main"
-URL="https://${SITE}/${USER_NAME}/${REPO_NAME}/raw/${BRANCH}"
-
 if [ -z "$1" ] ; then
   echo "Usage: ./$0 <path to file>"
   exit 1
 fi
 
+SITE="gitee.com"
+USER_NAME="objout"
+REPO_NAME="cs"
+BRANCH="main"
+URL="https://${SITE}/${USER_NAME}/${REPO_NAME}/raw/${BRANCH}"
 FILE_PATH="$1"
 
 curl --url $URL/$FILE_PATH -LO
