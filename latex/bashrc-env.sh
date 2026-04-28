b2132d65782771b1ff9d655c6fdde300ea2b734c
objout <android-ios@qq.com>
2026-04-28 17:46:31 +0800

    [U] latex/bashrc-env.sh

--- latex/bashrc-env.sh
+++ latex/bashrc-env.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# 和 latexmkrc 配合使用，将 LaTeX 的源代码和编译过程输出的文件分开存放
+# 使用共享内存
 #export TL_HOME=/run/user/$(id -u)/texlive
 export TL_HOME=/dev/shm/texlive
+
 if [ ! -d "$TL_HOME" ] ; then
   mkdir -p $TL_HOME/{var,tmp,out}
 fi
+
 export TEXMFVAR=$TL_HOME/var
+
+# 临时目录
 export TMP=$TL_HOME/tmp
 export TEMP=$TL_HOME/tmp
+
+# 设置编译输出目录
 export TL_BUILD_DIR=$TL_HOME/out
