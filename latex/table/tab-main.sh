6611194c5ea00cd4d5976614f40be2d21184d6de
objout <android-ios@qq.com>
2026-05-28 18:26:50 +0800

    [U] latex/table/tab-main.sh

--- latex/table/tab-main.sh
+++ latex/table/tab-main.sh
@@ -5,7 +5,7 @@ INS_TAB_SH_PATH=insert-tab.sh
 
 for file in $FILES ; do
   # chapter / section
-  chapter=$(echo $file | sed -e "s/[sc]\([0-9]\+\).tex/\1/")
+  chapter=$(basename $file | sed -e "s/[sc]\([0-9]\+\).tex/\1/")
   # 处理结果临时存放文件
   tmpfile=$(mktemp -p .)
 
